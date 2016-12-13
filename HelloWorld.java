6efb31cd176f579de90b1b880441396512c1ecc5
crsenna <cr.senna@gmail.com>
2016-12-13 11:05:39 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+//package myPackages.myHWClass;
+import myPackages.myHWClass.myHW1;
+import myPackages.myHWClass.myHW2;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+	// Program/version id
+	System.out.println("Hello World vr. 1.02 12.12.2016\n");
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+	myHW1.print("Hello, World");
+	myHW2.print("Hello, World");
+    }
+
+}
+
---
+++ README.rst
@@ -0,0 +1,19 @@
+Java Hello World program 
+
+Version 1.02 12.12.2016
+
+In this version, the program uses three classes to put messages on the screen.
+
+1. Directory structure
+   After correct installation, the directory structure of the program is:
+   
+
+2. Compiling 
+   To compile use hw-comp-all
+
+3. Executing
+   To run program use hw-run
+
+
+
+
