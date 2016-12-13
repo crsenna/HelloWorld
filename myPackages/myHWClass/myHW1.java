9261dceb6e3ef90ccbb37d36424e47a78b49f600
crsenna <cr.senna@gmail.com>
2016-12-13 11:09:43 +0000

    Add files via upload

---
+++ myPackages/myHWClass/myHW1.java
@@ -0,0 +1,11 @@
+package myPackages.myHWClass;
+
+public final class myHW1 {
+
+    public static void print(String msg) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println(msg.toUpperCase());
+    }
+
+}
+
---
+++ myPackages/myHWClass/myHW2.java
@@ -0,0 +1,11 @@
+package myPackages.myHWClass;
+
+public final class myHW2 {
+
+    public static void print(String msg) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println(msg.toLowerCase());
+    }
+
+}
+
